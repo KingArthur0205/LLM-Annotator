58d8e53fe9fb0aabec62f9ebc80b1a7599f2da09
KingArthur0205 <gpan20020205@gmail.com>
2025-03-16 22:51:11 +0000

    Added Refactored Data Loader

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 # Author: Arthur Pan(The University of Edinburgh)
-# Date: 2025/02/25
+# Date: 2025/03/16
 # Version: 0.1
 
 from argparse import ArgumentParser
 from config import ModelType
+
+from dataloader import DataLoader
 
 def parse_args():
     parser = ArgumentParser()
@@ -13,7 +15,8 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print(args)
+    dataloader = DataLoader(sheet_source="1miMC8M_UkfY_3XhglTAdt9sC9H8D6d9slzC7_LgcLOc",
+                            transcript_path=args.data_file)
 
 if __name__ == "__main__":
     main()
