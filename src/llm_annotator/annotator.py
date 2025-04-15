ab7b0d896961e872932c1450e9a7d34a6bd0d47f
KingArthur0205 <gpan20020205@gmail.com>
2025-04-15 18:15:25 +0100

    Minor Fix

--- src/llm_annotator/annotator.py
+++ src/llm_annotator/annotator.py
@@ -162,7 +162,7 @@ def process_requests(model_requests: Dict,
                      ) -> Dict:
     batches = {}
     for model, req_list in model_requests.items():
-        req_list = req_list[:100]
+        #req_list = req_list[:100]
 
         if model == "gpt-4o":
             batch = batch_openai_annotate(requests=req_list)
